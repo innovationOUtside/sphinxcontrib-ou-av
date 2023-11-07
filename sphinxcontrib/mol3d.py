cf6a8cc0cc773a346562d4dbd023b424d8ac5e8d
Tony Hirst <tony.hirst@gmail.com>
2023-11-07 16:33:48 +0000

    Update mol3d.py

--- sphinxcontrib/mol3d.py
+++ sphinxcontrib/mol3d.py
@@ -24,13 +24,16 @@ SUPPORTED_OPTIONS: List[str] = [
     "height",
     "width",
     "style",
+    "background",
     "query",
     "src",
 ]
 "List of the supported options attributes"
 
+
 class ou_mol3d(nodes.General, nodes.Element):
     """mol3d node."""
+
     pass
 
 
@@ -47,6 +50,7 @@ class mol3d(SphinxDirective):
         "query": directives.unchanged,
         "width": directives.unchanged,
         "height": directives.unchanged,
+        "background": directives.unchanged,
         "style": directives.unchanged,
         "src": directives.unchanged,
     }
@@ -61,29 +65,30 @@ class mol3d(SphinxDirective):
         _query = self.arguments[0]
         # Generate the asset
         import py3Dmol
+
         view = py3Dmol.view(query=_query)
         filename = f"{_query.replace(':','_')}_generated.html"
-        #view.setStyle({'cartoon':{'color':'spectrum'}})
+
+        # view.setStyle({'cartoon':{'color':'spectrum'}})
         # Style MUST be valid JSON
         style = self.options.get("style", '{"cartoon":{"color":"spectrum"}}')
-        print(f"style {style} was")
         view.setStyle(json.loads(style))
+        # Background
+        background = self.options.get("background", "0xeeeeee")
+        view.setBackgroundColor(background)
         os.makedirs("_tmp", exist_ok=True)
         tmp_path = os.path.join("_tmp", filename)
         view.write_html(tmp_path)
+        os.makedirs(env.app.builder.outdir, exist_ok=True)
         # Copy the media asset over to the build directory
         outpath = os.path.join(env.app.builder.outdir, filename)
-        dirpath = os.path.dirname(outpath)
-        if dirpath:
-            os.makedirs(dirpath, exist_ok=True)
         copyfile(tmp_path, outpath)
-        # TO DO - tidy up and clean _tmp/file
         _ou_mol3d = ou_mol3d(
-                query=_query,
-                height=self.options.get("height", ""),
-                width=self.options.get("width", ""),
-                src = tmp_path,
-            )
+            query=_query,
+            height=self.options.get("height", ""),
+            width=self.options.get("width", ""),
+            src=tmp_path,
+        )
         # TO DO - we need to define a <mol3d> tag handler for HTML
         # TO DO - or tweak this handler to render to everday HTML tags
         # The following is cribbed from Jupyter Book and adds a caption etc
@@ -99,16 +104,18 @@ class mol3d(SphinxDirective):
                 _ou_mol3d += caption
             if len(node) > 1:
                 _ou_mol3d += nodes.legend("", *node[1:])
-        return [
-            _ou_mol3d
-        ]
+        return [_ou_mol3d]
 
 
 def visit_ou_mol3d_html(translator: SphinxTranslator, node: ou_mol3d) -> None:
     """Entry point of the html mol3d node."""
     # start the mol3d block
     # TO DO - fix need for path hack
-    attr: List[str] = [f'{k}="{node[k].replace("_tmp/","")}"' for k in SUPPORTED_OPTIONS if k in node and node[k]]
+    attr: List[str] = [
+        f'{k}="{node[k].replace("_tmp/","")}"'
+        for k in SUPPORTED_OPTIONS
+        if k in node and node[k]
+    ]
     html: str = f"<iframe {' '.join(attr)}>"
 
     translator.body.append(html)
@@ -121,15 +128,13 @@ def depart_ou_mol3d_html(translator: SphinxTranslator, node: ou_mol3d) -> None:
 
 def visit_ou_mol3d_unsupported(translator: SphinxTranslator, node: ou_mol3d) -> None:
     """Entry point of the ignored mol3d node."""
-    logger.warning(
-        f"mol3d {node['query']}: unsupported output format (node skipped)"
-    )
+    logger.warning(f"mol3d {node['query']}: unsupported output format (node skipped)")
     raise nodes.SkipNode
 
 
 def setup(app: Sphinx) -> Dict[str, bool]:
     """Add mol3d node and parameters to the Sphinx builder."""
-    #app.add_config_value("mol3d_enforce_extra_source", False, "html")
+    # app.add_config_value("mol3d_enforce_extra_source", False, "html")
     app.add_node(
         ou_mol3d,
         html=(visit_ou_mol3d_html, depart_ou_mol3d_html),
