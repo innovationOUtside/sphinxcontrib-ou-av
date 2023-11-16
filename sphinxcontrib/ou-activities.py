f5c4edb17d5b21ea0635538be6dfe6c51c9bb2bd
Tony Hirst <tony.hirst@gmail.com>
2023-11-16 13:33:09 +0000

    Update activity html generation
    h/t Mark Hall's ou-book-theme

--- sphinxcontrib/ou-activities.py
+++ sphinxcontrib/ou-activities.py
@@ -2,23 +2,28 @@
 
 Originally based on https://github.com/sphinx-contrib/video/
 """
+from sphinx.transforms.post_transforms import SphinxPostTransform
 
 # TO DO - HTML outputs for exercise and activity
 
 from typing import Any, Dict, List
-
 from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.application import Sphinx
 from sphinx.util import logging
 from sphinx.util.docutils import SphinxDirective
-from sphinx_design.shared import create_component
+from sphinx_design.shared import create_component, is_component
 
-__author__ = "Raphael Massabot & Tony Hirst"
+from sphinxcontrib_ou_media.utils import hack_uuid, handle_css_js_assets
+
+__author__ = "Mark Hall & Tony Hirst"
 __version__ = "0.0.1"
 
-logger = logging.getLogger(__name__)
+# html_static_path = ["_static"]
+# html_css_files = ["ou_activities.css"]
+# html_js_files = ["ou_activities.js"]
 
+logger = logging.getLogger(__name__)
 
 SUPPORTED_OPTIONS: List[str] = [
     "timing",
@@ -26,7 +31,7 @@ SUPPORTED_OPTIONS: List[str] = [
 "List of the supported options attributes"
 
 
-class CustomInternalDirective(SphinxDirective):
+class OU_CustomInternalDirective(SphinxDirective):
     """Generic components..."""
 
     has_content = True
@@ -45,18 +50,104 @@ class CustomInternalDirective(SphinxDirective):
         return [component]
 
 
-class discussion(CustomInternalDirective):
+class OU_DiscussionDirective(OU_CustomInternalDirective):
     component_name = "ou-discussion"
 
 
-class answer(CustomInternalDirective):
+class OU_AnswerDirective(OU_CustomInternalDirective):
     component_name = "ou-answer"
 
 
-class CustomTopDirective(SphinxDirective):
+class OU_InteractionDirective(SphinxDirective):
+    """Generic components..."""
+
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    option_spec: Dict[str, Any] = {
+        "type": directives.unchanged,
+        "size": directives.unchanged,
+        "id": directives.unchanged,  # TO DO - validate id
+    }
+    final_argument_whitespace = True
+
+    def run(self):
+        component_name = "ou-interaction"
+        typ = self.options.get("type", "freeresponse")
+        size = self.options.get("size", None)
+        id = self.options.get("id", hack_uuid())
+        # TO DO  - complete options
+        if typ == "freeresponse":
+            if size not in ["paragraph"]:
+                size = "paragraph"
+        else:
+            typ_ = (
+                typ.split()[0]
+                .lower()
+                .replace("choice", "")
+                .replace("_", "")
+                .replace("-", "")
+            )
+            typ = typ_ if typ in ["multiple", "single"] else typ
+
+        component = create_component(
+            component_name,
+            classes=[component_name],
+            rawtext=self.content,
+            type=typ,
+            size=size,
+            id=id,
+        )
+        if not self.content:
+            self.state.nested_parse(self.content, self.content_offset, component)
+            return [component]
+
+        if typ in ["multiple", "single"]:
+            for item in self.content:
+                if not item.strip():
+                    continue
+                item_ = item.split()
+                # Drop the leading T/F
+                txt = " ".join(item_[1:]).strip()
+                # Get any feedback
+                item_ = txt.split("::")
+                txt = item_[0].strip()
+                feedback = item_[1].strip() if len(item_) == 2 else None
+                if item[0].upper() == "T":
+                    response = create_component(
+                        "Right",
+                        rawtext=txt,
+                        children=[nodes.Text(txt, txt)],
+                    )
+                    if feedback:
+                        response += create_component(
+                            "Feedback",
+                            rawtext=feedback,
+                            children=[nodes.Text(feedback, feedback)],
+                        )
+                    component += response
+                elif item[0].upper() == "F":
+                    response = create_component(
+                        "Wrong",
+                        rawtext=txt,
+                        children=[nodes.Text(txt, txt)],
+                    )
+                    if feedback:
+                        response += create_component(
+                            "Feedback",
+                            rawtext=feedback,
+                            children=[nodes.Text(feedback, feedback)],
+                        )
+                    component += response
+        else:
+            self.state.nested_parse(self.content, self.content_offset, component)
+        return [component]
+
+
+class OU_CustomActExDirective(SphinxDirective):
     """Generic top level activity/exercise directive.
 
-    Wrapper for the <exercise> tag embedding all the supported options
+    Generates basis for <Activity> and <Exercise> tags, embedding all the supported options
     """
 
     has_content: bool = True
@@ -88,20 +179,95 @@ class CustomTopDirective(SphinxDirective):
         return [activity]
 
 
-class activity(CustomTopDirective):
+class OU_ActivityDirective(OU_CustomActExDirective):
     component_name = "ou-activity"
 
 
-class exercise(CustomTopDirective):
+class OU_ExerciseDirective(OU_CustomActExDirective):
     component_name = "ou-exercise"
+
+
+# HtmlTransform classes from Mark Hall's ou-book-theme
+class ActivityAnswerHtmlTransform(SphinxPostTransform):
+    """Transform activity containers into the HTML specific AST structures."""
+
+    default_priority = 199
+    formats = ("html",)
+
+    def run(self):
+        """Run the transform"""
+        document: nodes.document = self.document
+        for node in document.findall(
+            lambda node: is_component(node, "ou-activity-answer")
+        ):
+            newnode = create_component(
+                "ou-activity-answer",
+                classes=["ou-activity-answer"],
+            )
+            newnode += nodes.raw("", "<hr/>", format="html")
+            newnode += nodes.raw(
+                "",
+                '<button class="sd-btn sd-btn-info ou-toggle ou-toggle-hidden"><span class="ou-toggle-show">Show answer</span><span class="ou-toggle-hide">Hide answer</span></button>',  # noqa: E501
+                format="html",
+            )
+            content_container = create_component(
+                "ou-activity-answer-content",
+                classes=["ou-activity-answer-content"],
+                children=node.children,
+            )
+            newnode += content_container
+            node.replace_self(newnode)
+
+
+class ActivityHtmlTransform(SphinxPostTransform):
+    """Transform activity containers into the HTML specific AST structures."""
+
+    default_priority = 198
+    formats = ("html",)
+
+    def run(self):
+        """Run the transform"""
+        document: nodes.document = self.document
+        for node in document.findall(lambda node: is_component(node, "ou-activity")):
+            newnode = create_component(
+                "ou-activity",
+                classes=["ou-activity"],
+            )
+            title_node = create_component(
+                "ou-activity-title",
+                classes=["ou-activity-title"],
+                children=node.children[0].children,
+            )
+            newnode += title_node
+            newnode += node.children[1:]
+            node.replace_self(newnode)
+
+
+# TO DO
+# right /wrong - SingleChoice, MultipleChoice; variants of a choice type?
+"""
+```{choice} single
+T: this
+F: that
+```
+T and F map on <Right> and <Wrong>
+"""
 
 
 def setup(app: Sphinx) -> Dict[str, bool]:
     """Add exercise node and parameters to the Sphinx builder."""
-    app.add_directive("ou-activity", activity)
-    app.add_directive("ou-exercise", exercise)
-    app.add_directive("ou-answer", answer)
-    app.add_directive("ou-discussion", discussion)
+    app.add_directive("ou-activity", OU_ActivityDirective)
+    app.add_directive("ou-exercise", OU_ExerciseDirective)
+    app.add_directive("ou-answer", OU_AnswerDirective)
+    app.add_directive("ou-discussion", OU_DiscussionDirective)
+    app.add_directive("ou-discussion", OU_DiscussionDirective)
+    app.add_directive("ou-interaction", OU_InteractionDirective)
+
+    app.add_post_transform(ActivityHtmlTransform)
+    app.add_post_transform(ActivityAnswerHtmlTransform)
+
+    # Pass in the stub filename used in static/js/STUB.js etc
+    handle_css_js_assets(app, "ou_activities")
 
     return {
         "parallel_read_safe": True,
--- sphinxcontrib_ou_media/utils.py
+++ sphinxcontrib_ou_media/utils.py
@@ -1,10 +1,11 @@
-import importlib.resources as resources
+from importlib import resources as import_resources
+import uuid
 
 
 def resources_path(path=None):
     """Get path to package resources."""
     path = path if path else "sphinxcontrib_ou_media"
-    return resources.files(path)
+    return import_resources.files(path)
 
 
 def fetch_template(*args, path=None):
@@ -16,3 +17,45 @@ def fetch_template(*args, path=None):
             return file.read()
     else:
         return None  # Handle the case where the file doesn't exist
+
+
+def hack_uuid():
+    while True:
+        # Generate a random UUID
+        uid = uuid.uuid4().hex
+        # There are conditions...
+        # - first character is not a digit
+        # - max 20 chars
+        if not uid[0].isdigit():
+            return uid[:20]
+
+
+from pathlib import Path
+from sphinx.util.fileutil import copy_asset
+import os
+
+
+def handle_css_js_assets(app, stub):
+    """Copy over CSS and JS assets to relevant directory
+    and add links to HTML page."""
+
+    source_dir = os.path.join(resources_path(), "static")
+    build_dir = os.path.join(app.outdir, "_static")
+
+    js_file = f"{stub}.js"
+    css_file = f"{stub}.css"
+
+    # Copy JavaScript file
+    copy_asset(
+        os.path.join(source_dir, "js", js_file),
+        build_dir,
+        app.builder,
+    )
+    # Copy CSS file
+    copy_asset(
+        os.path.join(source_dir, "css", css_file),
+        build_dir,
+        app.builder,
+    )
+    app.add_css_file(css_file)
+    app.add_js_file(js_file)
