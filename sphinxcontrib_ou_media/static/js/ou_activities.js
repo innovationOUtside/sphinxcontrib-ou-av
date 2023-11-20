00f30b823bd8684d8205a7fe9a5e86b3a5a2a0c8
Tony Hirst <tony.hirst@gmail.com>
2023-11-20 22:29:53 +0000

    ou0activities html assets css/js

--- sphinxcontrib_ou_media/static/js/ou_activities.js
+++ sphinxcontrib_ou_media/static/js/ou_activities.js
@@ -1,10 +1,16 @@
 /**
  * Enable the toggle button on OU activities.
  */
-export function activitySetup() {
+function activitySetup() {
     for(const toggle of document.querySelectorAll('.ou-toggle')) {
         toggle.addEventListener('click', () => {
             toggle.classList.toggle('ou-toggle-hidden');
         });
     }
 }
+
+// Add an event listener for 'DOMContentLoaded'
+document.addEventListener('DOMContentLoaded', () => {
+    // Call the function when the DOM is fully loaded
+    activitySetup();
+});
